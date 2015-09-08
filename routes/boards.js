45c39b3dc5b58d5c21f780774debe5e0a24b78b7
David Jones <unixmonkey1@gmail.com>
2015-09-08 18:14:30 -0400

    Connect to DB and build /boards JSON response

--- routes/boards.js
+++ routes/boards.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var pg = require('pg');
 var router = express.Router();
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/tracker-serverjs';
 
-/* GET users listing. */
+/* GET boards listing. */
 router.get('/', function(req, res, next) {
-  res.json({ data: [1, 3, { thing: "thingdata" }] });
+  var results = [];
+  pg.connect(connectionString, function(err, client, done) {
+    var query = client.query("SELECT * FROM boards ORDER BY position ASC;");
+    query.on('row', function(row) {
+      results.push(row);
+    });
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+    if(err) {
+      console.log(err);
+    }
+  });
 });
 
 module.exports = router;
