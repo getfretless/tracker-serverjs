e4bb0fbfd077e8d0e258b8b72aebd4274c98e9fa
Dave Strus <dave@davestrus.com>
2015-09-08 17:32:06 -0400

    Add database setup script.

--- db/setup.js
+++ db/setup.js
@@ -1,8 +1,12 @@
 var pg = require('pg');
+var fs = require('fs');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/tracker-serverjs';
 
 var client = new pg.Client(connectionString);
 client.connect();
 
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', function() { client.end(); });
+
+fs.readFile('db/structure.sql', 'utf8', function(err, data) {
+  var query = client.query(data);
+  query.on('end', function() { client.end(); });
+});
