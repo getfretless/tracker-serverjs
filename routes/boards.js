5b468ace952f71f7d8d3c7fc89bca76a4736603f
David Jones <unixmonkey1@gmail.com>
2015-09-09 13:00:50 -0400

    All the JSON

--- routes/boards.js
+++ routes/boards.js
@@ -6,10 +6,25 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/tr
 /* GET boards listing. */
 router.get('/', function(req, res, next) {
   var results = [];
+  var sql =
+    "select row_to_json(r) from (" +
+    "  select *, " +
+    "    ('http://localhost:3000/boards/' || boards.id) as url," +
+    "    (select array_to_json(array_agg(row_to_json(s))) from (" +
+    "        select *," +
+    "          ('http://localhost:3000/stories/' || stories.id) as url," +
+    "          (select row_to_json(u) from " +
+    "            (select id, name, email, created_at, updated_at from users where stories.user_id = users.id) u" +
+    "          ) as user" +
+    "        from stories where board_id = boards.id" +
+    "      ) s" +
+    "    ) as stories" +
+    "  from boards" +
+    ") r;";
   pg.connect(connectionString, function(err, client, done) {
-    var query = client.query("SELECT * FROM boards ORDER BY position ASC;");
+    var query = client.query(sql);
     query.on('row', function(row) {
-      results.push(row);
+      results.push(row.row_to_json);
     });
     query.on('end', function() {
       client.end();
