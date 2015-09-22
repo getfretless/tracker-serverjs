c22cd7328d458b2d307095bf44b5557bbccf881f
David Jones <unixmonkey1@gmail.com>
2015-09-22 14:53:57 -0400

    Add authenticate endpoint (but not actually authenticating yet)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var withConnection = require('../lib/withConnection');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/authenticate', function(req, res, next) {
+  var user;
+  var data = { email: req.body.email, password: req.body.password };
+  var sql = 'SELECT * FROM users WHERE email = $1 LIMIT 1;';
+  withConnection(function(client) {
+    client.query(sql, [data.email])
+      .on('row', function(row) {
+        user = row;
+      })
+      .on('end', function() {
+        client.end();
+        return res.json(user);
+      });
+  });
 });
 
 module.exports = router;
